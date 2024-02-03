8546af3f03fb79fefbb86fad1850b0af7ae97e71
Nempk1 <nempk2@hotmail.com>
2024-02-03 03:30:29 -0300

    added base command to reponde list of implemented commands(very bad func perf)

--- src/command_interp.c
+++ src/command_interp.c
@@ -3,13 +3,47 @@
 
 #include "irc_parse.h"
 #include "message.h"
+#include <string.h>
+
+static void*show_commands(void*);
 
 static struct command_str cmd_list[] = 
 {
+/* 	You can use the same command function
+ *  	with another cmd name as an 'alias'
+ *  	if place it one after the other you 
+ *  	get the (!cmd !cmd_alias) response
+ *  	from show_commands
+ */
 	{"!test", test_command, 0},
+ 	{"!test_alias", test_command, 0},
 	{"!mem_usage", memory_usage, 0},
+	{"!commands", show_commands, 0},
 	{NULL, NULL, 0}
 };
+
+static void *show_commands(void *data)
+{
+
+	struct twitch_msg *msg = data;
+	const struct command_str *list = cmd_list;
+	char buffer[1024];
+	while(list->cmd_str != NULL) {
+		if((list + 1)->fun_ptr == list->fun_ptr) {
+			strcat(buffer, "( ");
+			strcat(strcat(buffer, list->cmd_str), " ");
+			list++;
+			strcat(strcat(buffer, list->cmd_str), " ");
+			strcat(buffer, ") ");
+		}else 
+			strcat(strcat(buffer, list->cmd_str), " ");
+
+		list++;
+	}
+
+	respond_message(msg->conn,msg,buffer);
+	return NULL;
+}
 
 int cmd_interp(struct twitch_msg *msg)
 {
